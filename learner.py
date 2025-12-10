5b42b485da58eea451e4924f00e22321c98be459
Jaxon Zhang <yang.zhang.yz2483@yale.edu>
2025-12-10 00:18:08 -0500

    fix bugs

--- learner.py
+++ learner.py
@@ -3,6 +3,7 @@ import pytorch_lightning as pl
 import os
 import argparse
 
+from pathlib import Path
 from torch.optim import Adam
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from pytorch_lightning.callbacks import ModelCheckpoint, Callback
@@ -284,6 +285,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    base_dir = os.path.join("out", f"seed{SEED}_fold{args.fold}")
     run_name = args.model_name if args.mode == "baseline" else "anixplore"
     print(f"=== Task Name: {run_name} ===", flush=True)
 
@@ -292,7 +294,7 @@ if __name__ == "__main__":
     print_callbacks = PrintEpochResultCallback()
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath=os.path.join("out", "checkpoint", run_name),
+        dirpath=os.path.join(base_dir, "checkpoint", run_name),
         filename="{epoch}-{val_auc:.4f}",
         monitor="val_auc",
         mode="max",
@@ -300,11 +302,11 @@ if __name__ == "__main__":
         save_last=True,
         verbose=False,
     )
-    logger = CSVLogger(save_dir="out", name="logs", version=run_name)
+    logger = CSVLogger(save_dir=os.path.join(base_dir, "logs"), name=run_name)
 
     dm = AnimeIMDLDataModule(
         fake_root=args.fake_root,
-        real_root=args.real_root,
+        real_root=Path(args.real_root),
         fold=args.fold,
         img_size=IMG_SIZE,
         batch_size=BATCH_SIZE,
